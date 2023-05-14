e12ba54e07a07ddc163665bf51fc0f55af4625e7
MacroHard-Winux <129944538+MacroHard-Winux@users.noreply.github.com>
2023-05-14 07:51:22 +0000

    Winux 10 Go Public!

---
+++ winux10/download/shell_ie8.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+window.msCommonShellIE8 = { IE8: true }
+
+}
+/*
+     FILE ARCHIVED ON 14:51:50 Jul 29, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:29:18 May 03, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 250.062
+  exclusion.robots: 0.199
+  exclusion.robots.policy: 0.19
+  RedisCDXSource: 0.775
+  esindex: 0.009
+  LoadShardBlock: 229.048 (3)
+  PetaboxLoader3.datanode: 227.113 (4)
+  load_resource: 91.159
+  PetaboxLoader3.resolve: 44.909
+*/
---
+++ winux10/script2.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/*! Respond.js: min/max-width media query polyfill. Remote proxy (c) Scott Jehl. MIT/GPLv2 Lic. j.mp/respondjs  */
+(function(n,t,i){function h(t){return n.encodeURIComponent(t)}function a(i,r){function l(){var t;try{t=e.contentWindow.name}catch(i){}t?(e.src="about:blank",e.parentNode.removeChild(e),e=null,s&&(s=null,n.CollectGarbage&&n.CollectGarbage()),r(t)):n.setTimeout(l,100)}var e,s;"ActiveXObject"in n?(s=new ActiveXObject("htmlfile"),s.open(),s.write('<iframe id="x"><\/iframe>'),s.close(),e=s.getElementById("x")):(e=t.createElement("iframe"),e.style.cssText="position:absolute;top:-99em",u.insertBefore(e,u.firstElementChild||u.firstChild)),e.src=c(o)+"?url="+h(f)+"&css="+h(c(i)),n.setTimeout(l,500)}function c(n){var t=document.createElement("div"),i=n.split("&").join("&amp;").split("<").join("&lt;").split('"').join("&quot;");return t.innerHTML='<a href="'+i+'">x<\/a>',t.firstChild.href}function v(){if(~!f.indexOf(location.host)){var n=t.createElement("div");n.innerHTML='<a href="'+f+'"><\/a>',u.insertBefore(n,u.firstElementChild||u.firstChild),f=n.firstChild.href,n.parentNode.removeChild(n),n=null}}function y(){for(var i=t.getElementsByTagName("link"),n=0,r=i.length;n<r;n++){var u=i[n],f=i[n].href,e=/^([a-zA-Z:]*\/\/(www\.)?)/.test(f),o=l&&!e||e;u.rel.indexOf("stylesheet")>=0&&o&&function(n){a(f,function(t){n.styleSheet.rawCssText=t,respond.update()})}(u)}}var u=t.documentElement,l=t.getElementsByTagName("base")[0],p=[],w,e=i("#respond-proxy"),o=e?e.attr("href"):null,s=i("#respond-redirect"),f=s?s.attr("href"):location.href;o&&!respond.mediaQueriesSupported&&(v(),y())})(window,document,jQuery)
+
+}
+/*
+     FILE ARCHIVED ON 18:06:23 Jul 29, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 20:12:21 May 03, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 209.608
+  exclusion.robots: 0.165
+  exclusion.robots.policy: 0.153
+  cdx.remote: 0.058
+  esindex: 0.01
+  LoadShardBlock: 147.151 (3)
+  PetaboxLoader3.datanode: 114.288 (5)
+  load_resource: 223.993 (2)
+  PetaboxLoader3.resolve: 71.663 (2)
+*/
